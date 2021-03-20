b1ec04b71e99efc10c6730762f975470dba1fbb9
psa9 <saharipetteri8@gmail.com>
2021-03-20 23:13:50 +0200

    one line break removed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 docker build -t psa39/multi-client-k8s:latest -t psa39/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
 docker build -t psa39/multi-server-k8s:latest -t psa39/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
 docker build -t psa39/multi-worker-k8s:latest -t psa39/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
-
 docker push psa39/multi-client-k8s:latest
 docker push psa39/multi-server-k8s:latest
 docker push psa39/multi-worker-k8s:latest
