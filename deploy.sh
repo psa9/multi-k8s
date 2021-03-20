d5551ec29d5d31cbca656765958a4356e55613af
psa9 <saharipetteri8@gmail.com>
2021-03-20 17:34:02 +0200

    fixed some errors, copied the templates from course materials and changed variables

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t psa39/multi-client-k8s:latest -t psa39/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t psa39/multi-server-k8s-pgfix:latest -t psa39/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t psa39/multi-worker-k8s:latest -t psa39/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+
+docker push psa39/multi-client-k8s:latest
+docker push psa39/multi-server-k8s-pgfix:latest
+docker push psa39/multi-worker-k8s:latest
+
+docker push psa39/multi-client-k8s:$SHA
+docker push psa39/multi-server-k8s-pgfix:$SHA
+docker push psa39/multi-worker-k8s:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=psa39/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=psa39/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=psa39/multi-worker-k8s:$SHA
